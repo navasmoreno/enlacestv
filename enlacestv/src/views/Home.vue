ad5349b961da12b6d7b95ec09a6bc936e4be567e
Rodrigo <navas.moreno.devs@gmail.com>
2024-01-28 14:39:00 +0100

    Home

--- enlacestv/src/views/Home.vue
+++ enlacestv/src/views/Home.vue
@@ -1,10 +1,11 @@
 <template>
-    HOME
+  <div class="container is-fluid is-mobile mt-2" style="width: 100%;height: 100%;">
+    <div class="is-align-items-center is-flex is-justify-content-center" style="width: 100%;height: 100%;">
+      <div class="">
+        <div class="">
+          <img class="image" src="@/assets/logo.png" alt="Logo de la página"/>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
-<script>
-export default {
-  name: 'Home'
-}
-</script>
-<style>
-</style>
