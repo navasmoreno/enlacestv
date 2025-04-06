23ef2714b8ca918494272d126ccb63616f9f6b2e
Rodrigo <navas.moreno.devs@gmail.com>
2025-04-06 13:29:12 +0200

    Menu burger

--- enlacestv/src/views/Links.vue
+++ enlacestv/src/views/Links.vue
@@ -7,7 +7,7 @@
         </div>
       </div>
       <div class="column is-1 is-pulled-right">
-        <img class="image" src="@/assets/logo.png" />
+        <img class="image" src="@/assets/logo.png" style="min-width: 100px;"/>
       </div>
     </div>
     <div class="columns">
@@ -15,44 +15,48 @@
         <div class="">
           <section class="accordions">
             <article v-for="country in countries" class="accordion is-primary">
-              <div v-if="chanels.some(x => x.data.country.id == country.id)" class="accordion-header toggle">
-                <div class="has-text-left has-text-weight-bold is-size-4 pl-4 " style="width: 100%;">
-                  [{{ country.data.id }}] {{ country.data.label || "" }}
+              <div v-if="chanels.some(c => c.data.country.id == country.id && links.filter(l => l.data.chanel.id == c.id))">
+                <div class="accordion-header toggle">
+                  <div class="has-text-left has-text-weight-bold is-size-4 pl-4 " style="width: 100%;">
+                    [{{ country.data.id }}] {{ country.data.label || "" }}
+                  </div>
                 </div>
-              </div>
-              <div class="accordion-body">
-                <div class="accordion-content p-1">
-                  <div class="content">
-                    <div class="columns is-multiline is-mobile column-gap">
-                      <div v-for="chanel in chanels.filter(x => x.data.country.id == country.id)"
-                        class="column is-full-mobile is-half-tablet is-one-quarter-desktop">
-                        <div v-if="links.some(x => x.data.chanel.id == chanel.id)" class="card text-center">
-                          <div class="card-content p-2">
-                            <div class="card-header">
-                              <div class="card-header-title">
-                                <font-awesome-icon icon="fa-solid fa-desktop" class="mr-1" />
-                                {{ chanel.data.name }}{{ chanel.data.resolution.length > 0 ? `
-                                [${chanel.data.resolution}]` : '' }}
+                <div class="accordion-body">
+                  <div class="accordion-content p-1">
+                    <div class="content">
+                      <div class="columns is-multiline is-mobile column-gap">
+                        <div v-for="chanel in chanels.filter(x => x.data.country.id == country.id)"
+                          class="column is-full-mobile is-6-tablet is-4-desktop is-3-widescreen ">
+                          <div v-if="links.some(x => x.data.chanel.id == chanel.id)" class="card text-center">
+                            <div class="card-content p-2">
+                              <div class="card-header">
+                                <div class="card-header-title">
+                                  <font-awesome-icon icon="fa-solid fa-desktop" class="mr-1" />
+                                  {{ chanel.data.name }}{{ chanel.data.resolution.length > 0 ? `
+                                  [${chanel.data.resolution}]` : '' }}
+                                </div>
                               </div>
-                            </div>
-                            <div class="content pt-2">
-                              <div class="buttons">
-                                <div v-for="(link, index) in links.filter(x => x.data.chanel.id == chanel.id).sort((a,b)=> {
-                                  return a.data.feed_up-a.data.feed_down > b.data.feed_up-b.data.feed_down ? -1 : 1;
-                                })"
-                                  class="is-flex is-flex-wrap-wrap is-size-6 is-justify-content-center is-flex-grow-1">
-                                  <a class="button is-link is-light" :title="link.data.link" :href="link.data.link"
-                                    rel="nofollow">
-                                    <p class="mr-3 mb-0 is-uppercase">Enlace {{ index + 1 }}</p>
-                                    <font-awesome-icon icon="fa-solid fa-up-right-from-square" />
-                                  </a>
-                                  <div>
-                                    <button class="button is-medium is-success" v-on:click="setUp(link.id)" :title="link.data.feed_up">
-                                      <font-awesome-icon icon="fa-solid fa-thumbs-up" />
-                                    </button>
-                                    <button class="button is-medium is-danger" v-on:click="setDown(link.id)" :title="link.data.feed_down">
-                                      <font-awesome-icon icon="fa-solid fa-thumbs-down" />
-                                    </button>
+                              <div class="content pt-2">
+                                <div class="links buttons">
+                                  <div v-for="(link, index) in links.filter(x => x.data.chanel.id == chanel.id).sort((a, b) => {
+                                    return a.data.feed_up - a.data.feed_down > b.data.feed_up - b.data.feed_down ? -1 : 1;
+                                  })"
+                                    class="is-flex is-flex-wrap-wrap is-size-6 is-justify-content-center is-flex-grow-1">
+                                    <a class="button is-link is-light is-flex-grow-1" :title="link.data.link" :href="link.data.link"
+                                      rel="nofollow">
+                                      <p class="mr-3 mb-0 is-uppercase">Enlace {{ index + 1 }}</p>
+                                      <font-awesome-icon icon="fa-solid fa-up-right-from-square" />
+                                    </a>
+                                    <div class="feed">
+                                      <button class="button is-medium is-success" v-on:click="setUp(link.id)"
+                                        :title="link.data.feed_up">
+                                        <font-awesome-icon icon="fa-solid fa-thumbs-up" />
+                                      </button>
+                                      <button class="button is-medium is-danger" v-on:click="setDown(link.id)"
+                                        :title="link.data.feed_down">
+                                        <font-awesome-icon icon="fa-solid fa-thumbs-down" />
+                                      </button>
+                                    </div>
                                   </div>
                                 </div>
                               </div>
@@ -142,6 +146,16 @@ export default {
 
   .accordions .accordion.is-active .accordion-body {
     max-height: 100%;
+  }
+  @media (max-width: 1450px) {
+    .links.buttons {
+      div.feed {
+        width: 100%;
+        button {
+          width: 50%;
+        }
+      }
+    }
   }
 }
 </style>
